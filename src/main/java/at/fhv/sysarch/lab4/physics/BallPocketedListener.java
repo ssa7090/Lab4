2a1c72beec88274ec0765386a6621a4cd1d43624
ssa7090 <ssa7090@students.fhv.at>
2022-06-09 19:31:39 +0200

    get back pocketed balls

--- src/main/java/at/fhv/sysarch/lab4/physics/BallPocketedListener.java
+++ src/main/java/at/fhv/sysarch/lab4/physics/BallPocketedListener.java
@@ -3,5 +3,5 @@ package at.fhv.sysarch.lab4.physics;
 import at.fhv.sysarch.lab4.game.Ball;
 
 public interface BallPocketedListener {
-    public boolean onBallPocketed(Ball b);
+    public void onBallPocketed(Ball b);
 }
